01464ad21a461aae67ad83d13025b733afce579e
Eric Hogue <eric@erichogue.ca>
2010-06-05 17:40:18 -0400

    Added the contact page

---
+++ Public/application/controllers/ContactsController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * ContactsController Class File
+ *
+ * @category   PHP5
+ * @package    Koryukan
+ * @subpackage Public
+ * @author     Eric Hogue <eric@erichogue.ca>
+ * @copyright  2010 Gamma Entertainment. All Rights Reserved
+ * @license    http://www.gammae.com/license
+ * @version    SVN: $Id$
+ * @link       SVN: $HeadURL$
+ * @since      2010-06-05
+ *
+ */
+
+/**
+ * Contacts info
+ *
+ * @category   PHP5
+ * @package    Koryukan
+ * @subpackage Public
+ * @author     Eric Hogue <eric@erichogue.ca>
+ */
+class ContactsController extends Zend_Controller_Action
+{
+    /**
+     * Index
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        $pageTitle = $this->view->translate('Dojo & Contact Info');
+        $this->view->assign('pageTitle', $pageTitle);
+        $this->view->assign('sideImage', 'contacts.jpg');
+        $this->view->assign('sideImageAlt', $pageTitle);
+    }
+}
---
+++ Public/application/controllers/StoreController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * StoreController Class File
+ *
+ * @category   PHP5
+ * @package    Koryukan
+ * @subpackage Public
+ * @author     Eric Hogue <eric@erichogue.ca>
+ * @copyright  2010 Gamma Entertainment. All Rights Reserved
+ * @license    http://www.gammae.com/license
+ * @version    SVN: $Id$
+ * @link       SVN: $HeadURL$
+ * @since      2010-06-05
+ *
+ */
+
+/**
+ * StoreController
+ *
+ * @category   PHP5
+ * @package    package
+ * @subpackage subPackage
+ * @author     Eric Hogue <eric@erichogue.ca>
+ */
+class StoreController extends Zend_Controller_Action
+{
+    /**
+     * Index
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        $pageTitle = $this->view->translate('Online store');
+        $this->view->assign('pageTitle', $pageTitle);
+        $this->view->assign('sideImage', 'store' . $this->getRequest()->getParam('lang', 'en') . '.png');
+        $this->view->assign('sideImageAlt', $pageTitle);
+    }
+}
