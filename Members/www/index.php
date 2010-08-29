4799c0a125e8d392065171e9c67940a5a65f186c
Eric Hogue <eric@erichogue.ca>
2010-08-29 17:47:25 -0400

    Default env is not production

--- Members/www/index.php
+++ Members/www/index.php
@@ -6,7 +6,7 @@ defined('APPLICATION_PATH')
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'staging'));
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 defined('BASE_PATH')
     || define('BASE_PATH', realpath(APPLICATION_PATH . '/../../Base'));
